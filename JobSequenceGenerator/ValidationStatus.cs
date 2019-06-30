742d2547ed1e031a6cdbdd83f02c5dba48cfda36
Reishabh Saxena <reishabh9@hotmail.com>
2019-06-30 09:57:40 +0530

    Added the Data Validation enum and JobEngine-BAL Class

---
+++ JobSequenceGenerator/ValidationStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobProcessing
+{
+    public enum ValidationStatus
+    {
+        Success = 0,
+        No_Jobs = 1,
+        Invalid_Job = 2,
+        Jobs_Cannot_Depend_On_Themselves = 3,
+        Circular_Reference = 4
+    }
+}
